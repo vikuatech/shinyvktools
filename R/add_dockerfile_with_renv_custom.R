16f99a5505e01f47d5512e74e450e0425fb67561
rodserr <rodrigoserranom8@gmail.com>
2023-05-15 19:42:09 +0000

    secure_app + deployment helper

---
+++ R/add_dockerfile_with_renv_custom.R
@@ -0,0 +1,49 @@
+#' secure_app_vk
+#'
+#' @description Custom wrapper over golem::add_dockerfile_with_renv() that insludes .Renviron lines and .secrets in Dockerfile
+#'
+#' @param gcp_project name of GCP project
+#' @param ... argument to pass to golem::add_dockerfile_with_renv_custom()
+#'
+#' @return Creates or replace deploy/ folder with corresponding resources
+#'
+#' @noRd
+add_dockerfile_with_renv_custom <- function(gcp_project, ...){
+
+  golem::add_dockerfile_with_renv(output_dir="deploy", from = "rocker/shiny-verse", open = F, ...)
+
+  # Secret Service Account Token
+  secrets_files <- list.files('.secrets', full.names = F)
+  sat <- stringr::str_subset(secrets_files, '_sat.json')
+
+  # Edit the Dockerfile `deploy/Dockerfile` and add these lines so the app can get the token
+  env_lines <- readLines('.Renviron')
+  docker_lines <- readLines('deploy/Dockerfile')
+
+  new_env_lines <- paste('ENV', stringr::str_replace(env_lines, '.secrets/', '/root/'))
+  copy_sat_line <- paste('COPY', sat, '/root/')
+
+  writeLines(
+    c(docker_lines[1], copy_sat_line, new_env_lines, docker_lines[-1]),
+    'deploy/Dockerfile'
+  )
+
+  # Rename image name to GCP Build image
+  readme_lines <- readLines('deploy/README')
+
+  pkg_name <- stringr::str_extract(readme_lines[2], '(?<=-t ).*(?=:latest .)')
+  image_name <- paste('gcr.io', gcp_project, pkg_name, sep = '/')
+  new_readme_line <- stringr::str_replace(readme_lines[2], pkg_name, image_name)
+  run_line <- stringr::str_replace(readme_lines[3], pkg_name, image_name)
+  push_line <- sprintf('docker push %s:latest', image_name)
+
+  writeLines(
+    c(readme_lines[1], new_readme_line, run_line, readme_lines[4], push_line),
+    'deploy/README'
+  )
+
+  # Copy the service account token in the deploy folder
+  cat('Copying .secrets/', sat, 'into deploy/ \n')
+  file.copy(paste0('.secrets/', sat), paste0('deploy/', sat))
+
+}
--- R/secure_app_vk.R
+++ R/secure_app_vk.R
@@ -1,11 +1,17 @@
 #' secure_app_vk
 #'
-#' @description A fct function
+#' @description login module for vikua shiny apps
 #'
-#' @return The return value, if any, from executing the function.
+#' @param app_ui function application user-interface
+#' @param prod boolean identifying if the app communicates with BQ to ask for credentials and save logs
+#' @param tags_vk named list with the top, bottom and img strings to include in login module
+#' @param gcp_project,bq_dataset,bq_table string names of GCP resources to retrieve and write
+#' @param app_name string to include in logs
+#'
+#' @return invisible.
 #'
 #' @noRd
-secure_app_vk <- function(app_ui, prod = T){
+secure_app_vk <- function(app_ui, prod = T, tags_vk){
 
   if(prod){
     shinymanager::secure_app(
@@ -13,16 +19,16 @@ secure_app_vk <- function(app_ui, prod = T){
       id = "auth",
       tags_top =
         tags$div(
-          tags$h3("Vikua Platform: La Wawa Quoting App", style = "align:center"),
+          tags$h3(tags_vk$top, style = "align:center"),
           tags$img(
-            src = "https://storage.googleapis.com/vikua-styles/logos/logo_vikua_es_dark.png",
+            src = tags_vk$img,
             alt = 'logo_vikua', width = '150'
           )
         ),
       tags_bottom = tagList(
         tags$div(id = "placeholder-loading"),
         tags$div(
-          tags$p("Specialized interactive platform for La Wawa made by Vikua Corp")
+          tags$p(tags_vk$bottom)
         )
       ),
       background  = "linear-gradient(#272c30, #272c30);"
@@ -30,5 +36,84 @@ secure_app_vk <- function(app_ui, prod = T){
   }
   else{
     app_ui
+  }
+}
+
+#' @export
+#' @rdname secure_app_vk
+check_credentials_vk <- function(prod = T, gcp_project, bq_dataset, bq_table){
+
+  function(user, password){
+    user_ <- user
+    password_ <- password
+
+    # Aviso de Espera mientras se procesa login
+    # https://github.com/datastorm-open/shinymanager/issues/131
+    shiny::insertUI(
+      selector = "#placeholder-loading",
+      immediate = TRUE,
+      ui = tagList(
+        tags$div(
+          id = "loading-ui",
+          tags$div(
+            style = "text-align: center;",
+            "Authentication in progress, please wait..."
+          )
+        )
+      )
+    )
+
+    if(prod){
+      local_creds <- readr::read_csv('.secrets/credentials_temp.csv', show_col_types = FALSE)
+
+      res <- local_creds %>%
+        dplyr::filter(user == user_) %>%
+        dplyr::filter(password == password_)
+
+    } else{
+
+      .q <- sprintf('select password, permission, company from la_wawa.credentials where user = "%s" and password = "%s"', user_, password_)
+      res <- bigrquery::bq_project_query(gcp_project, .q) %>%
+        bigrquery::bq_table_download()
+
+    }
+
+    # Remove loading-ui text
+    shiny::removeUI(
+      selector = "#loading-ui",
+      immediate = TRUE
+    )
+
+    if (nrow(res) > 0) {
+      list(result = TRUE, user_info = list(user = user, permission = res$permission, company = res$company))
+    } else {
+      list(result = FALSE)
+    }
+
+  }
+
+}
+
+#' @export
+#' @rdname secure_app_vk
+log_store_custom_vk <- function(app_name, gcp_project, bq_dataset, bq_table, prod){
+  if(prod){
+    shinylogs::track_usage(
+      app_name = app_name,
+      storage_mode = shinylogs::store_custom(
+        function(logs) {
+
+          df <- tibble::tibble(logs = jsonlite::toJSON(logs), datetime = Sys.time())
+          bigrquery::bq_table(project = gcp_project, dataset = bq_dataset, table = bq_table) %>%
+            bigrquery::bq_table_upload(
+              df,
+              create_disposition = 'CREATE_IF_NEEDED',
+              write_disposition = 'WRITE_APPEND',
+              quiet = F,
+              fields = bigrquery::as_bq_fields(df)
+            )
+        }
+      )
+    )
   }
 }
