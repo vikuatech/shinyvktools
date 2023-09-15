77826e68385ba9db01b0b93f2ace43dc167e31c7
rodserr <rodrigoserranom8@gmail.com>
2023-09-15 15:50:39 +0000

    get_google_places wrapper

---
+++ R/get_google_places.R
@@ -0,0 +1,66 @@
+#' get_google_places
+#'
+#' @description Wrapper of googleway::get_places to paginate and get all possible results.
+#'
+#' @param ... parameters to pass to googleway::get_places
+#'
+#' @details
+#' get_google_places cleans the response to get a tibble of places and attributes:
+#' name, place_id, types, vicinity, rating, user_ratings_total, lat, lng
+#' get_google_places_pagination returns a list with max 3 responses (Google Places API return max 3 resposes of 20 places each)
+#'
+#' @return invisible.
+#'
+#' @export
+get_google_places <- function(...){
+
+tryCatch({
+  suppressWarnings({
+    places_list <- get_google_places_pagination(...)
+  })
+
+  places_list %>%
+    bind_rows() %>%
+    tidyr::unnest(geometry) %>%
+    tidyr::unnest(location) %>%
+    select(name, place_id, types, vicinity, rating, user_ratings_total, lat, lng)
+
+},
+error = function(e){
+  NA
+})
+
+}
+
+#' @export
+#' @rdname get_all_places
+get_google_places_pagination <- function(...){
+
+  places_list <- list()
+  page_token <- NULL
+  while (TRUE) {
+
+    tryCatch({
+      response <- google_places(..., page_token = page_token)
+    },
+    error = function(e){
+      response <- google_places(..., page_token = page_token)
+    })
+
+
+    places_list <- c(places_list, list(response$results))
+
+    # Si no hay más páginas, finaliza el bucle
+    if (is.null(response$next_page_token)) {
+      break
+    }
+
+    # Espera un momento para permitir que el token de página se active
+    Sys.sleep(2)
+
+    page_token <- response$next_page_token
+  }
+
+  return(places_list)
+}
+
